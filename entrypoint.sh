d02899c31bae563f32cf2e2d49b23bf7270c818e
udf <hellpl4y@gmail.com>
2023-01-04 01:36:29 +0500

    wip nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /myapp/tmp/pids/server.pid
+# rm -f /myapp/tmp/pids/server.pid
 
 bundle exec rails db:create db:migrate
 bundle exec rails assets:precompile
-bundle exec puma -C config/puma.rb
-
+# bundle exec puma -C config/puma.rb
+bundle exec rails s -b 0.0.0.0 -p 3000
