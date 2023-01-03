8c16210729bbb418b8fe0aacd4b6c7fbfd49b867
udf <hellpl4y@gmail.com>
2023-01-04 01:56:52 +0500

    wip nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-# rm -f /myapp/tmp/pids/server.pid
+rm -f /myapp/tmp/pids/server.pid
 
 bundle exec rails db:create db:migrate
 bundle exec rails assets:precompile
