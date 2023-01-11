7f2b2a45081f2a29799da746db916afe8fff1980
udf <hellpl4y@gmail.com>
2023-01-11 06:05:43 +0500

    fix ci

--- bin/tests.sh
+++ bin/tests.sh
@@ -4,8 +4,6 @@ set -e
 # wait until container entrypoint is done
 sleep 5
 
-until curl localhost:3000; do sleep 1; done
-
 docker compose -f docker-compose-tests.yml exec -e FIXTURES_PATH=spec/fixtures app rails db:fixtures:load
 docker compose -f docker-compose-tests.yml exec app bundle exec rspec
 docker compose -f docker-compose-tests.yml exec app bundle exec rubocop
