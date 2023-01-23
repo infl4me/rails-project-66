7f065167efbb988bbb63320f6ca5b96228cb5d73
udf <hellpl4y@gmail.com>
2023-01-23 08:00:56 +0500

    add translations

--- app/mailers/repository_check_mailer.rb
+++ app/mailers/repository_check_mailer.rb
@@ -6,6 +6,9 @@ class RepositoryCheckMailer < ApplicationMailer
     @repository_check = params[:repository_check]
     @check_output = ApplicationContainer[:repository_check_api].get_output(@repository_check)
 
-    mail(to: @user.email, subject: "Repository \"#{@repository_check.repository.full_name}\" report")
+    mail(
+      to: @user.email,
+      subject: t('repository_check_mailer.report_check_result.title', name: @repository_check.repository.full_name)
+    )
   end
 end
