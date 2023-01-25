704427dd33ed1bf5259aaea3ed894ec342937f47
udf <hellpl4y@gmail.com>
2023-01-25 05:04:28 +0500

    fix ci

--- app/jobs/repository_check_job.rb
+++ app/jobs/repository_check_job.rb
@@ -6,8 +6,6 @@ class RepositoryCheckJob < ApplicationJob
   def perform(repository_check)
     repository_check = CreateRepositoryCheckService.new.call(repository_check)
 
-    if repository_check.finished? && !repository_check.passed
-      RepositoryCheckMailer.with(user: repository_check.repository.user, repository_check:).report_check_result.deliver_now
-    end
+    RepositoryCheckMailer.with(user: repository_check.repository.user, repository_check:).report_check_result.deliver_now if repository_check.finished? && !repository_check.passed
   end
 end
